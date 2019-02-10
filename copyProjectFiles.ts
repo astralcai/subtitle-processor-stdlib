09387a2621492da84807c9323fd7260a9d1dade7
Astral Cai <astralcai@gmail.com>
2019-02-10 22:50:01 +0800

    Set up basic stdlib project structure with typescript

---
+++ copyProjectFiles.ts
@@ -0,0 +1,6 @@
+import * as shell from "shelljs";
+
+shell.cp("-R", "package.json", "dist/");
+shell.cp("-R", "env.json", "dist/");
+shell.cp("-R", "API.md", "dist/");
+shell.cp("-R", ".libignore", "dist/");
---
+++ src/functions/__main__.ts
@@ -0,0 +1,10 @@
+/**
+ * The primary entry point for this package is not used. A simple hello world function
+ * is here as a place-holder.
+ *
+ * @returns {string}
+ */
+
+module.exports = (context, callback) => {
+  callback(null, "hello world");
+};
